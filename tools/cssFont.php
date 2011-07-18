6b57eb74f0ced8d204a015eb2449e75c2f7239db
Anahkiasen <Anahkiasen@ee65ada7-bdf8-4fec-8cb3-3baef4c1a277>
2011-07-18 12:09:39 +0000

    Rétrocompatibilité cssFont Nomenclature archive fonctions deprecated

--- tools/cssFont.php
+++ tools/cssFont.php
@@ -1,10 +1,29 @@
 <?php
-function cssFont($queryFont)
+function cssFont($queryFont, $googleFont = true)
 {
 	if(!is_array($queryFont)) $queryFont = array($queryFont);
-	$fonts = implode(':extralight,light,regular,bold|', $queryFont);
-	$fonts = str_replace(' ', '+', $fonts);
-
-	echo '<link href="http://fonts.googleapis.com/css?family=' .$fonts. ':light,regular,bold" rel="stylesheet" type="text/css">';
+	
+	if($googleFont == true)
+	{
+		// Polices Google
+		$fonts = implode(':extralight,light,regular,bold|', $queryFont);
+		$fonts = str_replace(' ', '+', $fonts);
+	
+		echo '<link href="http://fonts.googleapis.com/css?family=' .$fonts. ':light,regular,bold" rel="stylesheet" type="text/css">';
+	}
+	else
+	{
+		// Polices @font-face
+		echo '<style type="text/css">';
+		foreach($queryFont as $value)
+			echo '
+			@font-face
+			{
+				font-family: \'' .$value. '\';
+				src: url(\'css/font/' .strtolower($value). '.eot\');
+				src: local(\'?\'), url(\'css/font/' .strtolower($value). '.woff\') format(\'woff\'), url(\'css/font/' .strtolower($value). '.ttf\') format(\'truetype\');
+			}';
+		echo '</style>';	
+	}
 }
 ?>
