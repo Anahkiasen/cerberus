18f376a0d7c83bdf6f7ed935cbe46463bc06dcd3
Anahkiasen <Anahkiasen@ee65ada7-bdf8-4fec-8cb3-3baef4c1a277>
2011-07-19 16:18:58 +0000

    /> manquant fonction cssFont

--- tools/cssFont.php
+++ tools/cssFont.php
@@ -9,7 +9,7 @@ function cssFont($queryFont, $googleFont = true)
 		$fonts = implode(':extralight,light,regular,bold|', $queryFont);
 		$fonts = str_replace(' ', '+', $fonts);
 	
-		echo '<link href="http://fonts.googleapis.com/css?family=' .$fonts. ':light,regular,bold" rel="stylesheet" type="text/css">';
+		echo '<link href="http://fonts.googleapis.com/css?family=' .$fonts. ':light,regular,bold" rel="stylesheet" type="text/css" />';
 	}
 	else
 	{
