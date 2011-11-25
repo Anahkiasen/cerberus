6cc24a068fa42785cc54435efedec572fdf15b45
Anahkiasen <Anahkiasen@ee65ada7-bdf8-4fec-8cb3-3baef4c1a277>
2011-11-25 08:12:24 +0000

    (fix) Adresse de la page de retour dans 404 (upd) TimThumb 191

--- include/404.php
+++ include/404.php
@@ -1,4 +1,5 @@
+<? global $desired; $arbre = $desired->get(); ?>
 <h1 style="margin:0">La page demandée n'existe pas</h1>
 
 <p>La page que vous avez demandé n'existe pas ou plus. Il est possible que vous ayez fait une erreur en tapant son nom.
-Nous vous conseillons de revenir <?= str::slink('home', 'à l\'accueil') ?> et de rententer d'accéder à la page.</p>
+Nous vous conseillons de revenir <?= str::slink(key($arbre), 'à l\'accueil') ?> et de rententer d'accéder à la page.</p>
