28a8caaaadb4e8df84c66c35549f1695cf6bd609
Anahkiasen <Anahkiasen@ee65ada7-bdf8-4fec-8cb3-3baef4c1a277>
2011-12-09 08:30:58 +0000

    (fix) Bug lors de l'implosion d'un array

--- include/admin.config.php
+++ include/admin.config.php
@@ -15,7 +15,7 @@ if(isset($_POST['db-host']))
 		$key = str_replace('-', '.', $key);
 		if(in_array($key, $arrays))
 		{
-			if(str::find(',', $value)) $formatted_value = implode("', '", $value);
+			if(str::find(',', $value)) $formatted_value = implode("', '", explode(',', $value));
 			$formatted_value = 'array(\'' .$formatted_value. '\')';
 		}
 		elseif(in_array($value, array('TRUE','FALSE'))) $formatted_value = $value;
