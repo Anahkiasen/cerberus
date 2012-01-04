b6e9c533c4ed875f99d90640cca4897f2c5692a3
Anahkiasen <Anahkiasen@ee65ada7-bdf8-4fec-8cb3-3baef4c1a277>
2012-01-04 09:35:41 +0000

    (fix) CSS des champs date en cas de maquette left/right

--- cerberus/class/core.update.php
+++ cerberus/class/core.update.php
@@ -25,7 +25,7 @@ class update
 			db::execute('ALTER TABLE  `cerberus_structure` ADD  `external_link` VARCHAR( 255 ) NOT NULL AFTER  `hidden`');
 			self::update(355);
 		}
-		self::update(372);
+		self::update(373);
 	}
 	
 	// Met à jour le numéro de révision
