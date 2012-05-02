0bf669d12ec96c8f4db03e5407dd38a83dfdce30
Anahkiasen <Anahkiasen@ee65ada7-bdf8-4fec-8cb3-3baef4c1a277>
2012-05-02 16:02:34 +0000

    (add) server::location() détermine le pays de la personne (mod) server::ip() récupère désormais l'IP de manière plus moderne (fix) Divers états des boutons Bootstrap

--- cerberus/class/kirby.server.php
+++ cerberus/class/kirby.server.php
@@ -13,8 +13,8 @@ class server
 	/**
 		* Gets a value from the _SERVER array
 		*
-		* @param	mixed		$key The key to look for. Pass false or null to return the entire server array. 
-		* @param	mixed		$default Optional default value, which should be returned if no element has been found
+		* @param  mixed    $key The key to look for. Pass false or null to return the entire server array. 
+		* @param  mixed    $default Optional default value, which should be returned if no element has been found
 		* @return mixed
 		*/	
 	static function get($key = FALSE, $default = NULL)
@@ -30,7 +30,29 @@ class server
 	 */
 	static function ip()
 	{
-		return self::get('REMOTE_ADDR');
+		$headers = array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR');
+		foreach($headers as $h)
+		{
+			if(array_key_exists($h, $_SERVER) === true)
+				foreach(explode(',', self::get($h)) as $ip)
+				{
+					if(filter_var($ip, FILTER_VALIDATE_IP) !== false)
+						return $ip;
+				}
+		}
 	}
+	
+	/**
+	 * Gets the person's current country and/or country code
+	 * 
+	 * @return array    An array of informations about the person's location
+	 */
+	static function location($ip = NULL)
+	{
+		if(!$ip) $ip = self::ip();
+		$country = file_get_contents('http://api.ipinfodb.com/v3/ip-country/?key=45af9a9ca62e018ed24abdb22adb47138ef319fa8227d6e1406c87fe7503b734&ip=' .$ip. '&format=json');
+		$country = str::parse($country, 'json');
+		return $country;
+   }
 }
 ?>
