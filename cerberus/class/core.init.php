96765960b53bb9dcf3b9dc5e3b7af667431d2267
Maxime Fabre <ehtnam6@gmail.com>
2012-05-11 14:56:25 +0200

    [add] Moved each action from init.php to a new init class Each of those actions can now be called in any order and are all facultatives (see @dependencies for more informations)

---
+++ cerberus/class/core.init.php
@@ -0,0 +1,306 @@
+<?php
+class init
+{
+	
+	//////////////////////////////////////////////////////////////
+	////////////////////////// FRAMEWORK ///////////////////////// 
+	//////////////////////////////////////////////////////////////
+	
+	//////////////////////////////////////////////////////////////
+	/////////////////////////// TOOLKIT ////////////////////////// 
+	//////////////////////////////////////////////////////////////
+	
+	//////////////////////////////////////////////////////////////
+	/////////////////////////// MODULES ////////////////////////// 
+	//////////////////////////////////////////////////////////////
+	
+	
+	/**
+	* Defines the current timezone
+	*/
+	function timeZone()
+	{
+		date_default_timezone_set('Europe/Paris');
+	}
+	
+	/**
+	* Defines the paths to some of the main folders
+	*/
+	function paths()
+	{	
+		// Define PATH_MAIN (root folder)
+		if(!defined('PATH_MAIN')) define('PATH_MAIN', NULL);
+		
+		// Define PATH_CORE (cerberus folder)
+		if(!defined('PATH_CORE')) define('PATH_CORE', PATH_MAIN.'cerberus/');
+		
+		// Define PATH_CONF (conf.php file)
+		if(!defined('PATH_CONF')) define('PATH_CONF', PATH_CORE.'conf.php');
+		
+		// Define PATH_CACHE (cerberus cache folder)
+		if(!defined('PATH_CACHE')) define('PATH_CACHE', PATH_CORE.'cache/');
+	}
+	
+	/**
+	* Handles and log errors
+	* @dependencies		paths
+	*/
+	function errorHandling()
+	{
+		// Loading errorHandle
+		include(PATH_CORE.'tools/errorHandle.php');
+		set_error_handler('errorHandle');
+
+		// Setting error level
+		error_reporting(E_ALL | E_STRICT ^ E_DEPRECATED);
+		
+		// Error logging
+		ini_set('error_log', PATH_CACHE.'error.log');
+		ini_set('log_errors', 'On');
+	}
+	
+	/**
+	* Assign autoloader as class loader
+	* @dependencies		paths
+	*/
+	function autoloader()
+	{
+		// Include the Autoloader and set it as main loader
+		include(PATH_CORE.'tools/classloader.php');
+		spl_autoload_register('__class_loader');
+	}
+	
+	/**
+	* Load the configuration file and set default values
+	* @dependencies		paths
+	*/
+	function config()
+	{
+		// Load the configuration default's value
+		config::set(config::$defaults);
+		
+		// Create the config file if it doesn't exist
+		if(!file_exists(PATH_CONF)) f::write(PATH_CONF, '<?'.PHP_EOL.'?>');
+				
+		// Load the local configuration file
+		else config::load(PATH_CONF);
+	}
+	
+	/**
+	* Define some of the main constants
+	* @dependencies		config,paths
+	*/
+	function constants()
+	{
+		// Page is local or not
+		if(!defined('LOCAL'))
+			define('LOCAL', (in_array(server::get('http_host'), array('localhost:8888', '127.0.0.1'))));
+		
+		// Setting parameters according to local or production
+		config::set(array(
+			'minify'    => !LOCAL,
+			'cache'     => !LOCAL,
+			'rewriting' => !LOCAL,
+			'local'     =>  LOCAL));
+				
+		// Main constants
+		if(!defined('REWRITING'))   define('REWRITING',   config::get('rewriting'));
+		if(!defined('MULTILANGUE')) define('MULTILANGUE', config::get('multilangue'));
+		if(!defined('CACHE'))
+		{
+			if(LOCAL or PATH_MAIN != NULL) define('CACHE', false); // If we're in local or in a subfolder
+			else                           define('CACHE', config::get('cache'));
+		}
+	}
+	
+	/**
+	* Connects to the configured database
+	* @dependencies		constants, config
+	*/
+	function mysql()
+	{
+		// If local, we set the SQL login informations to the local.variables
+		if(config::get('local.name'))
+		{
+			if(LOCAL) config::set(array(
+				'db.host'     => config::get('local.host'),
+				'db.user'     => config::get('local.user'),
+				'db.password' => config::get('local.password'),
+				'db.name'     => config::get('local.name')));
+				
+			// Unable to connect
+			if(!db::connect()) exit('Impossible d\'établir une connexion à la base de données');
+		}
+		
+		// Define the constant SQL to whether there's an existing connection
+		if(!defined('SQL')) define('SQL', db::connection(), FALSE);
+	}
+	
+	/**
+	* Logs the current user's environnement
+	* @dependencies		config, mysql, constants
+	*/
+	function stats()
+	{
+		// Avoiding main errors (no connection, or table, ...)
+		if(!config::get('logs') or !SQL) return false;
+		if(!db::is_table('cerberus_logs')) return false;
+		else update::table('cerberus_logs');
+		
+		// Getting the user's IP
+		$ip = server::ip();
+
+		// Checking if it's already in the database or note
+		if(db::field('cerberus_logs', 'ip', array('ip' => $ip)) and ($ip)) return false;
+		
+		// Getting informations
+		$ua      = browser::detect();
+		$domaine = url::domain();
+		$mobile  = (browser::mobile() or browser::ios()) ? 1 : 0;
+		
+		// Saving informations
+		if(!empty($ua['name']) and !empty($ua['platform']))
+			db::insert('cerberus_logs', array(
+				'ip'        => $ip,
+				'date'      => 'NOW()',
+				'platform'  => $ua['platform'],
+				'browser'   => $ua['name'],
+				'version'   => $ua['version'],
+				'engine'    => $ua['engine'],
+				'mobile'    => $mobile,
+				'locale'    => l::locale(),
+				'domaine'   => $domaine));
+	}
+	
+	/**
+	* Creates a list of requried files and folders if they aren't there
+	* @dependencies		dispatch, paths
+	*/
+	function required()
+	{
+		// List required files and their content
+		$required = array(
+			PATH_CACHE                                           => NULL,
+			dispatch::path(PATH_CERBERUS.'{images}/{plugins}/')  => NULL,
+			dispatch::path(PATH_CERBERUS.'{css}/{plugins}/')     => NULL,
+			dispatch::path(PATH_CERBERUS.'{js}/{plugins}/')      => NULL);
+			
+		// Add path to custom Sass environnement
+		if(LOCAL)
+			$required[dispatch::path(PATH_CERBERUS. '{sass}/base/_custom.sass')] = '@import ../../../../' .PATH_COMMON. 'sass/custom';
+		
+		// Create files and folders
+		foreach($required as $f => $content)
+		{
+			if(file_exists($f)) continue;
+			if(substr($f, -1) == '/') dir::make($f);
+			else f::write($f, $content);
+		}
+	}
+
+	/**
+	* Display debug informations
+	* @dependencies		navigation, constants
+	*/
+	function debug()
+	{
+		if(isset($_GET['cerberus_debug']))
+		{
+			// Get constants
+			$constantes = get_defined_constants(true);
+			$constantes = a::get($constantes, 'user');
+			
+			// Get main variables
+			$debug	= "[<strong>URL</strong>] " .url::current().'<br/>'.PHP_EOL;
+			$debug .= "[<strong>PAGE</strong>] " .navigation::current().'<br/>'.PHP_EOL;
+			$debug .= "[<strong>LANGUE</strong>] " .l::current().'<br/>'.PHP_EOL;
+			
+			// Display superglobals
+			if($_GET) $debug .= "[<strong>GET</strong>]\n\n<pre>" .print_r($_GET, true). '</pre>'.PHP_EOL;
+			if($_POST) $debug .= "[<strong>POST</strong>]\n\n<pre>" .print_r($_POST,true). '</pre>'.PHP_EOL;
+			if($_SESSION) $debug .= "[<strong>SESSION</strong>]\n\n<pre>" .print_r($_SESSION, true). '</pre>';
+			if($constantes) $debug .= "[<strong>CONSTANTES</strong>]\n\n<pre>" .print_r($constantes, true). '</pre>';
+			
+			// Echo if local, hide in code if online
+			echo LOCAL
+				? '<div class="cerberus_debug">' .$debug. '</div>'
+				: '<p style="display:none">' .str::unhtml($debug). '</p>';
+		}
+	}
+
+	/**
+	* Puts the current page in cache
+	* @dependencies		navigation, constants
+	*/
+	function cache()
+	{
+		if(CACHE)
+		{
+			// If the user hasn't already set some parameters
+			if(!isset($setCache)) $setCache = array();
+			
+			// Set default parameters
+			$setCache['name'] = a::get($setCache, 'name', navigation::current());
+			$setCache['cache_time'] = a::get($setCache, 'cache_time');
+			$setCache['cache_get_variables'] = a::get($setCache, 'cache_get_variables', true);
+			$setCache['get_remove'] = a::get($setCache, 'get_remove', array('page', 'pageSub', 'PHPSESSID', 'langue', 'gclid', 'cerberus_debug'));
+			$setCache['type'] = 'html';
+			
+			// Determine if we should cache the page or not
+			// Exceptions are : page is administration ; page is set as not cached in the database ; in the $setCache
+			// Default is true
+			if(navigation::$page == 'admin') $caching = FALSE;
+			elseif(SQL and db::is_table('cerberus_structure'))
+				$caching = db::field('cerberus_structure', 'cache', db::where(array('CONCAT_WS("-",parent,page)' => $setCache['name'], 'parent' => $setCache['name']), 'OR'));
+			
+			if(!isset($caching)) $caching = a::get($setCache, 'cache', TRUE);
+			
+			// Start the output buffer
+			if($caching) $start = cache::page($setCache['name'], $setCache);
+		}
+	}
+
+	/**
+	* Initialize the dispatch class
+	*/
+	function dispatch()
+	{
+		new dispatch();
+	}
+	
+	/**
+	* Updates the Cerberus core
+	*/
+	function update()
+	{
+		new update();
+	}
+	
+	/**
+	* Backup the database
+	* @dependencies		mysql,constants,cerberus
+	*/
+	function backup()
+	{
+		if(db::connection() and CACHE and function_exists('backupSQL')) backupSQL();
+	}
+	
+	/**
+	* Loading Cerberus modules
+	*/
+	function modules()
+	{
+		new Cerberus(config::get('cerberus'));
+	}
+	
+	function language()
+	{
+		new l();
+	}
+	
+	function navigation()
+	{
+		new navigation();
+	}
+}
+?>
--- cerberus/init.php
+++ cerberus/init.php
@@ -1,222 +1,83 @@
 <?php
-// Gestion des erreurs
-include('tools/errorHandle.php');
-date_default_timezone_set('Europe/Paris');
-ini_set('error_log', 'cerberus/cache/error.log');
-ini_set('log_errors', 'On');
+// Including bootstrap file
+include('class/core.init.php');
+$cerberus = new init();
 
-// Chemins principaux
-if(!defined('PATH_MAIN')) define('PATH_MAIN', '');
-if(!defined('PATH_CONF')) define('PATH_CONF', PATH_MAIN.'cerberus/conf.php');
+// Setting main path constants
+$cerberus->paths();
 
-// Chargement du moteur Cerberus
-include(PATH_MAIN.'cerberus/class/kirby.request.php');
-function __class_loader($class_name) 
-{
-	$class_name = str_replace('_', '.', strtolower($class_name));
-	$file = glob(PATH_MAIN.'cerberus/class/{kirby,class,core,kirby.plugins}.' .$class_name. '*.php', GLOB_BRACE);
-	if($file and file_exists($file[0]) and !class_exists($class_name))
-	{
-		require_once($file[0]); 
-		if(method_exists($class_name, 'init')) 
-			call_user_func(array($class_name, 'init')); 
-		return true;
-	}
-}
-spl_autoload_register('__class_loader');
+// Defining and assigning class __autoloader
+$cerberus->autoloader();
+
+// Error handling
+$cerberus->errorHandling();
+
+// Timezone setting
+$cerberus->timeZone();
+
+// Starting a new session
 session::start();
+
 content::start();
 
-/*
-########################################
-############# ENVIRONNEMENT ############
-########################################
-*/
-
-// Configuration du site
-if(!defined('LOCAL')) define('LOCAL', (in_array(server::get('http_host'), array('localhost:8888', '127.0.0.1'))));
-config::set(config::$defaults);
-if(!file_exists(PATH_CONF)) f::write(PATH_CONF, '<?'.PHP_EOL.'?>');
-else config::load(PATH_CONF);
-
-// Paramètres local/production
-config::set(array(
-	'minify' =>    !LOCAL,
-	'cache' =>     !LOCAL,
-	'rewriting' => !LOCAL,
-	'local' =>      LOCAL));
+	// Loading config file
+	$cerberus->config();
+	
+	// Setting main constants
+	$cerberus->constants();
+	
+	$cerberus->dispatch();
+	// Connecting to database
+	$cerberus->mysql();
+	
+	// Mise à jour du moteur
+	$cerberus->update();
+	
+	// Log user stats
+	$cerberus->stats();
+	
+	// Setting cache manifest if existing
+	$manifest = (CACHE and file_exists('cache.manifest') and config::get('cache.manifest')) ? 'manifest="cache.manifest"' : NULL;
+	
+	// Adding browser sniffing (I know) to the html tag
+	echo '<!DOCTYPE html>'.PHP_EOL;
+	echo '<html ' .$manifest. ' class="' .browser::css(). '">'.PHP_EOL;
+	
+	content::start();
+	
+		// Create required files and folders
+		$cerberus->required();
 		
-// Constantes
-if(!defined('REWRITING'))     define('REWRITING',   	config::get('rewriting'));
-if(!defined('MULTILANGUE'))   define('MULTILANGUE', 	config::get('multilangue'));
-if(!defined('CACHE'))
+		// Loading translations index
+		$cerberus->language();
+		
+		// Loading navigation structure
+		$cerberus->navigation();
+		
+		// Display debug informations 
+		$cerberus->debug();
+		
+		// Cache the page
+		$cerberus->cache();
+		
+		// Loading cerberus modules
+		$cerberus->modules();
+		
+		// Backing up database
+		$cerberus->backup();
+		
+// -------------------------------------------------- */
+
+/**
+	* Shortcut for r::get()
+	*
+	* @param	 mixed		$key The key to look for. Pass false or null to return the entire request array. 
+	* @param	 mixed		$default Optional default value, which should be returned if no element has been found
+	* @return	mixed
+	* @package Kirby
+	*/	
+function get($key = false, $default = NULL)
 {
-	if(LOCAL or PATH_MAIN != NULL)  define('CACHE', false);
-	else                            define('CACHE', config::get('cache'));
+	return r::get($key, $default);
 }
-
-// Affichage et gestion des erreurs
-error_reporting(E_ALL | E_STRICT ^ E_DEPRECATED);
-set_error_handler('errorHandle');
-
-// Gestion des ressources et chemins
-new dispatch();
-
-/*
-########################################
-############# CONNEXION SQL ############
-########################################
-*/
-
-// Connexion à la base de données
-if(config::get('local.name'))
-{
-	if(LOCAL) config::set(array(
-		'db.host' => 		config::get('local.host'),
-		'db.user' => 		config::get('local.user'),
-		'db.password' => 	config::get('local.password'),
-		'db.name' => 		config::get('local.name')));
-	if(!db::connect()) exit('Impossible d\'établir une connexion à la base de données');
-}
-if(!defined('SQL')) define('SQL', db::connection(), FALSE);
-
-// Mise à jour du moteur
-new update();
-
-/*
-########################################
-############# STATISTIQUES #############
-########################################
-*/
-
-$ip = server::ip();
-if(SQL and config::get('logs'))
-{
-	if(db::is_table('cerberus_logs'))
-	{
-		if(!db::field('cerberus_logs', 'ip', array('ip' => $ip)) and ($ip))
-		{
-			$ua = browser::detect();
-			$domaine = url::domain();
-			$mobile = (browser::mobile() or browser::ios()) ? 1 : 0;
-			
-			if(!empty($ua['name']) and !empty($ua['platform']))
-				db::insert('cerberus_logs', array(
-					'ip'       =>  $ip,
-					'date'     =>  'NOW()',
-					'platform' => 	$ua['platform'],
-					'browser'  => 	$ua['name'],
-					'version'  => 	$ua['version'],
-					'engine'   => 	$ua['engine'],
-					'mobile'   => 	$mobile,
-					'locale'   =>  l::locale(),
-					'domaine'  => 	$domaine));
-		}
-	}
-	else update::table('cerberus_logs');
-}
-
-/*
-########################################
-########### EN-TÊTE DU SITE ############
-########################################
-*/
-
-$manifest = (CACHE and file_exists('cache.manifest') and config::get('cache.manifest')) ? 'manifest="cache.manifest"' : NULL;
-
-// Ajout des balises HTML averc leur selecteur correct
-echo '<!DOCTYPE html>'.PHP_EOL;
-echo '<html ' .$manifest. ' class="' .browser::css(). '">'.PHP_EOL;
-content::start();
-
-/*
-########################################
-########### FICHIERS REQUIS ############
-########################################
-*/
-
-// Fichiers/dossiers requis
-$required = array(
-	PATH_CACHE                                           => NULL,
-	dispatch::path(PATH_CERBERUS.'{images}/{plugins}/')  => NULL,
-	dispatch::path(PATH_CERBERUS.'{css}/{plugins}/')     => NULL,
-	dispatch::path(PATH_CERBERUS.'{js}/{plugins}/')      => NULL);
-	
-// Fichier de variables SASS
-if(LOCAL)
-	$required[dispatch::path(PATH_CERBERUS. '{sass}/base/_custom.sass')] = '@import ../../../../' .PATH_COMMON. 'sass/custom';
-
-// Création des fichiers/dossiers
-foreach($required as $f => $content)
-{
-	if(file_exists($f)) continue;
-	if(substr($f, -1) == '/') dir::make($f);
-	else f::write($f, $content);
-}
-
-/*
-########################################
-######### LANGUE & NAVIGATION ##########
-########################################
-*/
-
-// Gestion des langues et de la navigation
-new l();
-new navigation();
-
-/*
-########################################
-####### DEBUG ET CONSTANTES ############
-########################################
-*/
-
-// Affichage des superglobales pour debug
-if(isset($_GET['cerberus_debug']))
-{
-	$constantes = get_defined_constants(true);
-	$constantes = a::get($constantes, 'user');
-	
-	$debug  = "[<strong>URL</strong>] " .url::current().'<br/>'.PHP_EOL;
-	$debug .= "[<strong>PAGE</strong>] " .navigation::current().'<br/>'.PHP_EOL;
-	$debug .= "[<strong>LANGUE</strong>] " .l::current().'<br/>'.PHP_EOL;
-	if($_GET) $debug .= "[<strong>GET</strong>]\n\n<pre>" .print_r($_GET, true). '</pre>'.PHP_EOL;
-	if($_POST) $debug .= "[<strong>POST</strong>]\n\n<pre>" .print_r($_POST,true). '</pre>'.PHP_EOL;
-	if($_SESSION) $debug .= "[<strong>SESSION</strong>]\n\n<pre>" .print_r($_SESSION, true). '</pre>';
-	if($constantes) $debug .= "[<strong>CONSTANTES</strong>]\n\n<pre>" .print_r($constantes, true). '</pre>';
-	
-	echo LOCAL
-		? '<div class="cerberus_debug">' .$debug. '</div>'
-		: '<p style="display:none">' .str::unhtml($debug). '</p>';
-}
-
-/*
-########################################
-############# MISE EN CACHE ############
-########################################
-*/
-
-if(CACHE)
-{
-	// Paramètres préexistants
-	if(!isset($setCache)) $setCache = array();
-	$setCache['name'] = a::get($setCache, 'name', navigation::current());
-	$setCache['cache_time'] = a::get($setCache, 'cache_time');
-	$setCache['cache_get_variables'] = a::get($setCache, 'cache_get_variables', true);
-	$setCache['get_remove'] = a::get($setCache, 'get_remove', array('page', 'pageSub', 'PHPSESSID', 'langue', 'gclid', 'cerberus_debug'));
-	$setCache['type'] = 'html';
-	
-	// Autoriser le caching
-	if(navigation::$page == 'admin') $caching = FALSE;
-	elseif(SQL and db::is_table('cerberus_structure'))
-		$caching = db::field('cerberus_structure', 'cache', db::where(array('CONCAT_WS("-",parent,page)' => $setCache['name'], 'parent' => $setCache['name']), 'OR'));
-	if(!isset($caching)) $caching = TRUE;
-	if(isset($setCache['cache'])) $caching = $setCache['cache'];
-	
-	// Démarrage de la mise en cache
-	if($caching) $start = cache::page($setCache['name'], $setCache);
-}
-
-// Chargement des modules Cerberus
-$cerberus = new Cerberus(config::get('cerberus'));
-if(db::connection() and CACHE and function_exists('backupSQL')) backupSQL();
 ?>
