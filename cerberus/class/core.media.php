bbc316e81bd30627ac01fa567920fb307f47e334
Maxime Fabre <ehtnam6@gmail.com>
2012-05-23 10:56:48 +0200

    [del] Remove createNivo and replace with media::slideshow

--- cerberus/class/core.media.php
+++ cerberus/class/core.media.php
@@ -36,6 +36,39 @@ class media extends dispatch
 		}
 			
 		return PATH_CORE.'class/svn.timthumb.php?src=' .$file. '&' .a::glue($params, '&', '=');
+	
+	/**
+	 * Creates a slideshow (currently based on nivoSlider)
+	 * 
+	 * @param string  $folder  The folder containing the images to use
+	 * @param int     $width   The width of the slideshow
+	 * @param int     $height  The height of the slideshow
+	 * @param mixed   $params  The parameters to pass, as a JSON or PHP array
+	 * @param boolean $shuffle Whether PHP should shuffle the images found in the directory or not
+	 */
+	static function slideshow($folder, $width, $height, $params = array(), $shuffle = true)
+	{
+		// Create a HTML-safe id for the block
+		$id_block = str::slugify($folder);
+		
+		// Fetch the pictures
+		$images = glob(PATH_FILE.$folder.'/*.jpg');
+		echo PATH_FILE.$folder.'/*.jpg';
+		a::show($images);
+		if($shuffle) shuffle($images);
+		
+		// Creating the block
+		echo "<div id='" .$id_block. "' style='height: " .$height. "px; max-width: " .$width. "px; margin: auto'>".PHP_EOL;
+		foreach($images as $i) echo "\t".
+			str::img(
+				self::timthumb(
+					$folder.f::name($i),
+					$width,
+					$height)).PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		
+		// Add the Javascript code to the page
+		dispatch::plugin('nivoSlider', '#'.$id_block, $params);
 	}
 	
 	/**
