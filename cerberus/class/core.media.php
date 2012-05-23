bfcbecaa4419b49fc12dce90ceddc7a3ceeafafa
Maxime Fabre <ehtnam6@gmail.com>
2012-05-23 10:59:12 +0200

    [mov] Move TimThumb into plugins/ subfolder

--- cerberus/class/core.media.php
+++ cerberus/class/core.media.php
@@ -35,7 +35,8 @@ class media extends dispatch
 				: $file;
 		}
 			
-		return PATH_CORE.'class/svn.timthumb.php?src=' .$file. '&' .a::glue($params, '&', '=');
+		return PATH_CORE.'class/plugins/timthumb.php?src=' .$file. '&' .a::glue($params, '&', '=');
+	}
 	
 	/**
 	 * Creates a slideshow (currently based on nivoSlider)
