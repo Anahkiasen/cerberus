c83f00f4e0bb1237c9f150b9e8b27dd081a4a422
Maxime Fabre <ehtnam6@gmail.com>
2012-05-25 09:44:22 +0200

    [fix] Path to the Timthumb cache directory

--- cerberus/class/plugins/timthumb-config.php
+++ cerberus/class/plugins/timthumb-config.php
@@ -1,6 +1,6 @@
 <?php
 if(!defined('FILE_CACHE_SUFFIX'))          define('FILE_CACHE_SUFFIX', '.jpg');
-if(!defined('FILE_CACHE_DIRECTORY'))       define('FILE_CACHE_DIRECTORY', '../../cerberus/cache/timthumb');
+if(!defined('FILE_CACHE_DIRECTORY'))       define('FILE_CACHE_DIRECTORY', '../../../cerberus/cache/timthumb');
 if(!defined('DEFAULT_Q'))                  define('DEFAULT_Q', 80);
-if(!defined('ALLOW_ALL_EXTERNAL_SITES'))   define ('ALLOW_ALL_EXTERNAL_SITES', true);		
+if(!defined('ALLOW_ALL_EXTERNAL_SITES'))   define ('ALLOW_ALL_EXTERNAL_SITES', true);
 ?>
