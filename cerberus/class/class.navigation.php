80caa92ab0b06b93225180c835a88ac86fa6f6f9
Maxime Fabre <ehtnam6@gmail.com>
2012-06-03 02:13:02 +0200

    [mod] Allow customization of page author in navigation::footer

--- cerberus/class/class.navigation.php
+++ cerberus/class/class.navigation.php
@@ -241,12 +241,12 @@ class navigation
 	}
 
 	// Pied de page
-	static function footer($links = array())
+	static function footer($links = array(), $author = 'Le Principe de Stappler', $author_link = 'http://www.stappler.fr/')
 	{
 		$footer = '&copy;Copyright ' .date('Y');
 		if(config::get('sitename')) $footer .= ' - ' .config::get('sitename');
 		if(SQL and db::is_table('cerberus_structure')) $footer .= ' - ' .str::slink('sitemap', l::get('menu-sitemap'));
-		$footer .= ' - Conception : ' .str::link('http://www.stappler.fr/', 'Le Principe de Stappler');
+		$footer .= ' - Conception : ' .str::link($author_link, $author);
 		if(isset(self::$data['contact']['submenu']['legales'])) $footer .= ' - ' .str::slink('contact-legales', l::get('menu-contact-legales'));
 		if(isset(self::$data['contact']['submenu']['contact'])) $footer .= ' - ' .str::slink('contact', l::get('menu-contact'));
 		if(!empty($links))
