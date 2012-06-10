e0f9f7acee4775202019724a8f693d8eb1197b4b
Maxime Fabre <ehtnam6@gmail.com>
2012-06-10 10:13:58 +0200

    [mod] Ensure Dispatch module is loaded before trying to add CSS/JS

--- cerberus/close.php
+++ cerberus/close.php
@@ -4,16 +4,19 @@
  * Adds some Cerberus magic into the page, cache it, and display it
  */
 
-$body = content::end(true);
+$modifiedBody = content::end(true);
 
 // Add Javascript to the body tag
-$modifiedBody = str_replace('</body>', dispatch::getJS(true).'</body>', $body);
+if($init->loaded('dispatch'))
+{
+	$modifiedBody = str_replace('</body>', dispatch::getJS(true).'</body>', $modifiedBody);
+	dispatch::getCSS();
+}
 
 // Append current page to the body classes
 $modifiedBody = preg_replace('#<body( class="(.+)")?>#', '<body class="' .navigation::css(). ' $2">', $modifiedBody);
 
 // Add the head tag
-dispatch::getCSS();
 $modifiedBody = str_replace('<head>', null, $modifiedBody);
 $modifiedBody = head::header().$modifiedBody;
 
