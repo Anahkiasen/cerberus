00ad15293754e4906cf30a1ac9bc74bf74667068
Maxime Fabre <ehtnam6@gmail.com>
2012-06-13 11:50:37 +0200

    [add] Tests for kirby.cookie

---
+++ cerberus/tests/cookieTest.php
@@ -0,0 +1,36 @@
+<?php
+ob_start();
+
+require('../class/core.init.php');
+new Init('test', '../../');
+
+class cookieTest extends PHPUnit_Framework_TestCase
+{
+	public function testSetCookie()
+	{
+		cookie::set('testCookie', 'testValue');
+
+		self::assertArrayHasKey('testCookie', $_COOKIE);
+		self::assertEquals($_COOKIE['testCookie'], 'testValue');
+
+		cookie::remove('testCookie');
+	}
+
+	public function testGetCookie()
+	{
+		cookie::set('testCookie2', 'testValue');
+
+		$result = cookie::get('testCookie2');
+		self::assertEquals($result, 'testValue');
+
+		cookie::remove('testCookie2');
+	}
+
+	public function testRemoveCookie()
+	{
+		cookie::set('testCookie3', 'testValue');
+
+		cookie::remove('testCookie3');
+		self::assertArrayNotHasKey('testCookie3', $_COOKIE);
+	}
+}
