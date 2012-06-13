8dc5dbc1673bd29e094f8c58b71e79184c734d93
Maxime Fabre <ehtnam6@gmail.com>
2012-06-13 12:18:26 +0200

    [mod] Use ob_get_clean instead of ob_get_contents and ob_end_clean in content::end

--- cerberus/class/kirby.content.php
+++ cerberus/class/kirby.content.php
@@ -11,54 +11,53 @@
 class content
 {
 	/**
-		* Starts the output buffer
-		*
-		*/
+	 * Starts the output buffer
+	 *
+	 */
 	public static function start()
 	{
 		ob_start();
 	}
 
 	/**
-		* Stops the output buffer
-		* and flush the content or return it.
-		*
-		* @param	boolean	$return Pass true to return the content instead of flushing it
-		* @return mixed
-		*/
+	 * Stops the output buffer and flush the content or return it.
+	 *
+	 * @param  boolean $return Pass true to return the content instead of flushing it
+	 * @return mixed
+	 */
 	public static function end($return = false)
 	{
 		if($return)
 		{
-			$content = ob_get_contents();
-			ob_end_clean();
+			$content = ob_get_clean();
 			return $content;
 		}
 		ob_end_flush();
 	}
 
 	/**
-		* Loads content from a passed file
-		*
-		* @param	string	$file The path to the file
-		* @param	boolean $return True: return the content of the file, false: echo the content
-		* @return mixed
-		*/
+	 * Loads content from a passed file
+	 *
+	 * @param  string  $file   The path to the file
+	 * @param  boolean $return True: return the content of the file, false: echo the content
+	 * @return mixed
+	 */
 	public static function load($file, $return = true)
 	{
 		self::start();
-		require_once($file);
+			require_once($file);
 		$content = self::end(true);
+
 		if($return) return $content;
 		echo $content;
 	}
 
 	/**
-		* Simplifies setting content type headers
-		*
-		* @param	string	$ctype The shortcut for the content type. See the keys of the $ctypes array for all available shortcuts
-		* @param	string	$charset The charset definition for the content type header. Default is "utf-8"
-		*/
+	 * Simplifies setting content type headers
+	 *
+	 * @param  string $ctype   The shortcut for the content type. See the keys of the $ctypes array for all available shortcuts
+	 * @param  string $charset The charset definition for the content type header. Default is "utf-8"
+	 */
 	public static function type()
 	{
 		$args = func_get_args();
@@ -66,19 +65,21 @@ class content
 		// shortcuts for content types
 		$ctypes = array(
 			'html' => 'text/html',
-			'css'	=> 'text/css',
-			'js'	 => 'text/javascript',
-			'jpg'	=> 'image/jpeg',
-			'png'	=> 'image/png',
-			'gif'	=> 'image/gif',
+			'css'  => 'text/css',
+			'js'   => 'text/javascript',
+			'jpg'  => 'image/jpeg',
+			'png'  => 'image/png',
+			'gif'  => 'image/gif',
 			'json' => 'application/json'
 		);
 
-		$ctype	 = a::get($args, 0, c::get('content_type', 'text/html'));
-		$ctype	 = a::get($ctypes, $ctype, $ctype);
-		$charset = a::get($args, 1, c::get('charset', 'utf-8'));
+		$ctype   = a::get($args, 0, config::get('content_type', 'text/html'));
+		$ctype   = a::get($ctypes, $ctype, $ctype);
+		$charset = a::get($args, 1, config::get('charset', 'utf-8'));
 
-		header('Content-type: ' . $ctype . '; charset=' . $charset);
+		$header = 'Content-type: ' .$ctype. '; charset=' .$charset;
+		header($header);
 
+		return $header;
 	}
 }
