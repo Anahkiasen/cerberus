b895eaccd87b1c20d46fe34003af8d20d2d50c24
Maxime Fabre <ehtnam6@gmail.com>
2012-06-13 11:49:54 +0200

    [fix] Deletion of cookies in cookie::remove

--- cerberus/class/kirby.cookie.php
+++ cerberus/class/kirby.cookie.php
@@ -10,14 +10,14 @@
 class cookie
 {
 	/**
-		* Set a new cookie
-		*
-		* @param	string	$key The name of the cookie
-		* @param	string	$value The cookie content
-		* @param	int		 $expires The number of seconds until the cookie expires
-		* @param	string	$domain The domain to set this cookie for.
-		* @return boolean true: the cookie has been created, false: cookie creation failed
-		*/
+	 * Set a new cookie
+	 *
+	 * @param  string  $key     The name of the cookie
+	 * @param  string  $value   The cookie content
+	 * @param  int     $expires The number of seconds until the cookie expires
+	 * @param  string  $domain  The domain to set this cookie for.
+	 * @return boolean Whether the cookie has been successfully created or not
+	 */
 	public static function set($key, $value, $expires = 3600, $domain = '/')
 	{
 		if(is_array($value)) $value = a::json($value);
@@ -26,27 +26,27 @@ class cookie
 	}
 
 	/**
-		* Get a cookie value
-		*
-		* @param	string	$key The name of the cookie
-		* @param	string	$default The default value, which should be returned if the cookie has not been found
-		* @return mixed	 The found value
-		*/
+	 * Get a cookie value
+	 *
+	 * @param  string $key     The name of the cookie
+	 * @param  string $default The default value, which should be returned if the cookie has not been found
+	 * @return mixed  The found value
+	 */
 	public static function get($key, $default = null)
 	{
 		return a::get($_COOKIE, $key, $default);
 	}
 
 	/**
-		* Remove a cookie
-		*
-		* @param	string	$key The name of the cookie
-		* @param	string	$domain The domain of the cookie
-		* @return mixed	 true: the cookie has been removed, false: the cookie could not be removed
-		*/
+	 * Remove a cookie
+	 *
+	 * @param  string $key    The name of the cookie
+	 * @param  string $domain The domain of the cookie
+	 * @return mixed  true: the cookie has been removed, false: the cookie could not be removed
+	 */
 	public static function remove($key, $domain = '/')
 	{
-		$_COOKIE[$key] = false;
-		return @setcookie($key, false, (time() - 3600), $domain);
+		$_COOKIE = a::remove($_COOKIE, $key);
+		return @setcookie($key, null, (time() - 3600), $domain);
 	}
 }
