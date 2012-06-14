780ed985190501cce3d722cc9a8a8f740fbc73c9
Maxime Fabre <ehtnam6@gmail.com>
2012-06-14 02:02:21 +0200

    [fix] Correct validTest::testPassword

--- cerberus/tests/validTest.php
+++ cerberus/tests/validTest.php
@@ -1,3 +1,23 @@
 <?php
 class validTest extends PHPUnit_Framework_TestCase
 {
+	public function passwords()
+	{
+		return array(
+			array('pass'),
+			array('password'),
+			array('password513'),
+			array('98')
+			);
+	}
+
+	/**
+	 * @data Provider passwords
+	 */
+	public function testPassword($password = null)
+	{
+		$valid = v::password($password);
+		if(strlen($password) < 4) self::assertFalse($valid);
+		else self::assertTrue($valid);
+	}
+}
