de056d05b4b50fd3c4e5ceff4bdde1665fa433cd
Maxime Fabre <ehtnam6@gmail.com>
2012-06-14 12:25:34 +0200

    [test] Use bogus config file for tests

--- cerberus/tests/phpunit.php
+++ cerberus/tests/phpunit.php
@@ -1,5 +1,10 @@
 <?php
+// Avoid headers problem
 ob_start();
 
+// Bogus config file
+define('PATH_CONF', 'tests.json');
+
+// Load Init class
 if(!class_exists('Init')) require('cerberus/class/core.init.php');
 new Init('test');
