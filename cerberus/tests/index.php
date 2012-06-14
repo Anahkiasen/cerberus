06946c0f75947bfaf2a6d0a454752b42f8dc0467
Maxime Fabre <ehtnam6@gmail.com>
2012-06-14 11:26:29 +0200

    [mod] Add custom styles to the Unit Testing page

--- cerberus/tests/index.php
+++ cerberus/tests/index.php
@@ -5,8 +5,12 @@ require('../init.php');
 // Setting title
 head::title('Unit Testing Summary');
 
+// Main styles
+dispatch::assets('!styles', 'jquery', 'unit-testing');
+dispatch::googleFonts('Open Sans:100,400,700', 'Raleway:100');
+
 // Reading available JSON tests
-$json = parseTests('phpunit.json');
+$json   = parseTests('phpunit.json');
 $suites = readTests($json);
 
 // Parsing the misconstructed JSON reports (fuckers)
@@ -66,44 +70,61 @@ function readTests($tests)
 </head>
 
 <body>
-	<div class="container">
+	<section id="corps">
 		<h1>Unit Testing Results</h1>
+
+		<h2>Table of contents</h2>
+		<ul id ="toc">
+			<?php
+			foreach($suites as $title => $osef)
+				echo '<li>' .str::link('#'.str::slugify($title), str::remove('Test', $title)). '</li>';
+			?>
+		</ul>
+
 		<?php
 		$pass = a::extract($suites, 'errors');
 		$pass = array_sum($pass);
 		$color = $pass == 0 ? 'success' : 'error';
-		str::display('Number of errors found : ' .$pass, $color);
+		str::display(
+			str::img('https://secure.travis-ci.org/Anahkiasen/cerberus.png').
+			' Number of errors found : ' .$pass, $color);
 		$suites = a::remove($suites, 'errors');
 		?>
 
 		<?php
 		foreach($suites as $title => $tests)
 		{
+			if(sizeof($tests) == 1) continue;
+			$testCount = sizeof($tests) - 1;
+			$passed = $testCount - intval(a::get($tests, 'errors'));
+
 			$strippedTitle = str::remove('Test', $title);
-			echo '<h2 style="clear:both">' .$strippedTitle. '</h2><div class="row">';
+			echo '<section id="' .str::slugify($title). '">';
+				echo '<h2>' .$strippedTitle. ' (' .$passed. '/' .$testCount. ')</h2>'.PHP_EOL;
 
-			foreach($tests as $name => $infos)
-			{
-				if($name == 'errors' or $name == 'title') continue;
-				$name     = str::remove($title.'::test', $name);
-				$dataSet  = preg_replace('/(.+) with data set #([0-9]+) \(.+\)/is', '$2', $name);
-				$provider = preg_replace('/(.+) with data set #([0-9]+) \((.+)\)/is', '$3', $name);
-				$name     = preg_replace('/with data set #([0-9]+) \((.+)\)/', null, $name);
-				$message  = a::get($infos, 'message');
+				foreach($tests as $name => $infos)
+				{
+					if($name == 'errors' or $name == 'title') continue;
+					$name     = str::remove($title.'::test', $name);
+					$dataSet  = preg_replace('/(.+) with data set #([0-9]+) \(.+\)/is', '$2', $name);
+					$provider = preg_replace('/(.+) with data set #([0-9]+) \((.+)\)/is', '$3', $name);
+					$name     = preg_replace('/with data set #([0-9]+) \((.+)\)/', null, $name);
+					$message  = a::get($infos, 'message');
 
-				if(!$message) $message = a::get($infos, 'status') ? 'Success' : 'Error';
+					if(!$message) $message = a::get($infos, 'status') ? 'Success' : 'Error';
 
-				echo '<div class="span3"><h3>' .$name. '</h3>';
-				if($provider != $name) echo str::display('#' .$dataSet.' -> ('.$provider.')', 'info');
-				echo a::get($infos, 'status')
-					? str::display($message, 'success')
-					: str::display($message, 'error');
-				echo '</div>';
-			}
-			echo '</div>';
+					echo '<article>'.PHP_EOL;
+					echo '<h3>' .$name. '</h3>'.PHP_EOL;
+					if($provider != $name) echo str::display('#' .$dataSet.' -> ('.$provider.')', 'info');
+					echo a::get($infos, 'status')
+						? str::display($message, 'success')
+						: str::display($message, 'error');
+					echo '</article>'.PHP_EOL;
+				}
+			echo '</section>';
 		}
 		?>
-	</div>
+	</section>
 </body>
 </html>
 <?php require('../close.php');
