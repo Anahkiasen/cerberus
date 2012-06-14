3ea6cfbe5868ba4916d6a7e3967eafd86c265f2e
Maxime Fabre <ehtnam6@gmail.com>
2012-06-14 13:20:15 +0200

    [del] Rollback to per-class gestion of config file

--- cerberus/tests/phpunit.php
+++ cerberus/tests/phpunit.php
@@ -2,9 +2,6 @@
 // Avoid headers problem
 ob_start();
 
-// Bogus config file
-define('PATH_CONF', 'tests.json');
-
 // Load Init class
 if(!class_exists('Init')) require('cerberus/class/core.init.php');
 new Init('test');
