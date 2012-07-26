5f7546d022bd50d1192e026637da866077b87d89
Anahkiasen <ehtnam6@gmail.com>
2012-07-26 17:49:00 +0200

    [add] Vendor::compass generates a Compass configuration file

--- librairies/Toolkit/Vendor.php
+++ librairies/Toolkit/Vendor.php
@@ -8,11 +8,12 @@
  */
 namespace Cerberus\Toolkit;
 
-use Cerberus\Core\Dispatch;
+use Cerberus\Core\Dispatch,
+    Cerberus\Toolkit\File;
 
 class Vendor
 {
-  // Webfonts ---------------------------------------------------------------- /
+  // Webfonts ------------------------------------------------------ /
 
   /**
    * Adds embed code for a Typekit account
@@ -40,7 +41,7 @@ class Vendor
     Dispatch::stylesheet('http://fonts.googleapis.com/css?family=' .$fonts);
   }
 
-  // Statistics -------------------------------------------------------------- /
+  // Statistics ---------------------------------------------------- /
 
   /**
    * Add a Google Analytics account
@@ -59,5 +60,87 @@ class Vendor
       "var s=document.getElementsByTagName('script')[0];" .
       "s.parentNode.insertBefore(ga,s)})();"
       );
+  }
+
+  // Assets -------------------------------------------------------- /
+
+  /**
+   * Generates a Compass configuration file
+   *
+   * @param  string  $writePath The folder to write it (defaults to root)
+   * @param  array   $config    Configuration options
+   * @param  array   $plugins   Additional Rubygems to load
+   * @return boolean            Success in creating the file or not
+   */
+  public static function compass($writePath = null, $configuration = array(), $plugins = array())
+  {
+    // Check if the file already exists
+    if(file_exists($writePath.'compass.rb')) return true;
+
+    // Create an empty file
+    $file = null;
+
+    // Default configuration and plugins
+    $defaults = array(
+      '0'                  => 'Folders',
+        'project_path'     => 'public/',
+        'images_dir'       => 'img',
+        'css_dir'          => 'css',
+        'javascripts_dir'  => 'js',
+        'fonts_dir'        => 'fonts',
+
+      '1'                  => 'Options',
+        'output_style'     => ':expanded',
+        'preferred_syntax' => ':sass',
+        'line_comments'    => 'false',
+        'relative_assets'  => 'true',
+
+      '2'                  => 'Extensions'
+    );
+    $defaultsPlugins = array(
+      'susy',
+      'animate',
+      'rgbapng',
+      'modular-scale',
+      'normalize'
+    );
+
+    // Merge with given configuration parameters
+    $configuration = array_merge($configuration, $defaults);
+    $plugins       = array_merge($plugins, $defaultsPlugins);
+
+    // Writing options
+    foreach ($configuration as $k => $v)
+    {
+      // If value is comment
+      if (is_numeric($k))
+      {
+        if(!empty($file)) $file .= PHP_EOL;
+        $file .= '# ' . $v . PHP_EOL;
+        continue;
+      }
+
+
+      // If value is array
+      elseif(is_array($v))
+        $v = json_encode($v);
+
+      // If value is neither a boolean nor a symbol
+      elseif(
+        !($v == 'true' or
+          $v == 'false' or
+          substr($v, 0, 1) == ':')
+      ) $v = '"' . $v . '"';
+
+      // Else, just print value
+      $file .= $k . ' = ' . $v . PHP_EOL;
+    }
+
+    // Loading extensions
+    foreach($plugins as $e)
+      $file .= "require '" .$e. "'" . PHP_EOL;
+
+    // Write core configuration file in root
+    File::write($writePath.'compass.rb', $file);
   }
 }
