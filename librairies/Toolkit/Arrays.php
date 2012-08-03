0df587841593265d1fae9759972fde4f11cb196a
Maxime Fabre <ehtnam6@gmail.com>
2012-08-03 16:59:31 +0200

    [add] Arrays::pluck extract a column from an array

--- librairies/Toolkit/Arrays.php
+++ librairies/Toolkit/Arrays.php
@@ -13,6 +13,10 @@ use Cerberus\Toolkit\String;
 
 class Arrays
 {
+  ////////////////////////////////////////////////////////////////////
+  ////////////////////////////// ACTIONS /////////////////////////////
+  ////////////////////////////////////////////////////////////////////
+
   /**
    * Gets an element of an array by key
    *
@@ -80,12 +84,26 @@ class Arrays
   /**
    * Removes an element by value
    *
-     * @param  array   $array  The source array
-     * @param  mixed   $search The value to look for
-     * @return array           The result array without the removed element
+   * @param  array   $array  The source array
+   * @param  mixed   $search The value to look for
+   * @return array           The result array without the removed element
    */
   public function removeValue($array, $search)
   {
     return self::remove($array, $search, false);
+  }
+
+  /**
+   * Extracts a single column from an array
+   *
+   * @param  array  $array The source array
+   * @param  string $key The key name of the column to extract
+   * @return array  The result array with all values from that column.
+   */
+  public static function pluck($array, $key)
+  {
+    return array_map(function($v) use ($key) {
+      return is_object($v) ? $v->$key : $v[$key];
+    }, $array);
   }
 }
