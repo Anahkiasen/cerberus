fa8b7a261c83692b4b4a446737ef4306a9bfe4f2
Maxime Fabre <ehtnam6@gmail.com>
2012-09-04 10:03:03 +0200

    [add] Macro HTML::responsive() to activate responsive viewport

--- start.php
+++ start.php
@@ -16,9 +16,16 @@ $test = Language::locale();
 
 // Custom Cerberus macros ------------------------------------------ /
 
+// Favicon
 HTML::macro('favicon', function($favicon)
 {
   return "<link href='" .URL::to_asset($favicon). "' rel='shortcut icon' />";
+});
+
+// Responsive design
+HTML::macro('responsive', function()
+{
+  return '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
 });
 
 // Post-processing ------------------------------------------------- /
