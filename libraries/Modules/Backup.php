1c556f2d1ac3afc37d572033ad4ff686c4623dd9
Maxime Fabre <ehtnam6@gmail.com>
2012-09-05 18:03:55 +0200

    [add] Backup classes to save database

---
+++ libraries/Modules/Backup.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ *
+ * Backup
+ *
+ * Does backup of the database, manage them
+ * clean them and load them back
+ */
+
+namespace Cerberus\Modules;
+
+use \Alert,
+    Cerberus\Toolkit\Directory,
+    Cerberus\Toolkit\File,
+    Laravel\Database as DB;
+
+class Backup
+{
+  /**
+   * Path where the dumps will be saved
+   * @var string
+   */
+  private $storage = null;
+
+  /**
+   * PDO Object
+   * @var PDO
+   */
+  private $pdo;
+
+  /**
+   * The current date
+   * @var string
+   */
+  private $date;
+
+  /**
+   * Whether debug messages should be printed or not
+   * @var boolean
+   */
+  public $debug = false;
+
+  /**
+   * Initialize the Backup class
+   */
+  public function __construct()
+  {
+    // Set correct storage path
+    $this->storage = path('storage').'sql/';
+
+    // Create folder if it doesn't exist
+    if(!file_exists($this->storage)) Directory::make($this->storage);
+
+    // Create PDO Object
+    $this->pdo = DB::connection()->pdo;
+
+    // Cache current date
+    $this->date = date('Y-m-d');
+  }
+
+  /**
+   * Save the current database if necessary
+   *
+   * @return boolean Whether the database was saved or not
+   */
+  public function save()
+  {
+    $tables = $this->tables();
+    $savedTables = 0;
+
+    // If we have tables to save
+    if ($tables) {
+
+      // Get database name
+      $database = DB::connection()->config['database'];
+      $this->debug('info', 'Saving database `' .$database. '`');
+
+      // Read dumps for current date
+      $dumps = $this->readDumps();
+      if (!empty($dumps)) {
+        $this->debug(
+          'success',
+          'A dump for today (' .$this->date. ') already exists (' .sizeof($dumps). ' tables in memory).');
+
+        return true;
+      }
+
+      // If no dumps, save tables
+      $folder = $this->getFolderForDate();
+      foreach ($tables as $table) {
+        $filepath = $folder.$table.'_'.$this->date.date('@H-m-s').'.sql';
+        list($export, $numberRows) = $this->exportTable($table);
+
+        $write = File::write($filepath, $export);
+        if ($write) {
+          $this->debug('success', 'Table ' .$table. ' saved successfully (' .$numberRows. ' rows)');
+          $savedTables++;
+        } else {
+          $this->debug('error', 'An error occured saving table `' .$table. '`');
+        }
+      }
+
+      // Make sure all tables were correctly saved
+      if ($savedTables == sizeof($tables)) {
+        $this->debug('success', 'Database saved successfully');
+      }
+    } else {
+      $this->debug('info', 'No tables to save');
+
+      return true;
+    }
+  }
+
+  /**
+   * List all the dumps from a folder
+   *
+   * @param  string $folder A folder path
+   * @return array          An array of dumps available
+   */
+  public function readDumps($folder = null)
+  {
+    if(!$folder) $folder = $this->getFolderForDate();
+
+    // If the folder doesn't exist, return empty array
+    if(!file_exists($folder)) return array();
+
+    // Fetch all dumps for that date
+    $dumps = glob($folder.'*.sql');
+
+    return $dumps;
+  }
+
+  /**
+   * Export a table in SQL format
+   * @param  string $table The table name
+   * @return array         The SQL dump ; Number of rows in table
+   */
+  public function exportTable($table)
+  {
+    // Add premptive DROP TABLE
+    $dump = null;
+    $dump .= 'DROP TABLE IF EXISTS `' .$table. '`;'.PHP_EOL;
+
+    // Fetch creation query for this table
+    $showCreate = $this->pdo->query('SHOW CREATE TABLE `' .$table. '`')->fetch(\PDO::FETCH_NUM);
+    $dump .= $showCreate[1].';'.PHP_EOL;
+
+    // Fetch the table's content
+    $tableContent = $this->pdo->query('SELECT * FROM '.$table)->fetchAll(\PDO::FETCH_ASSOC);
+
+    // Create INSERT lines
+    $numberInserts = 0;
+    if ($tableContent) {
+      $rows = array_keys($tableContent[0]);
+      $numberInserts = sizeof($tableContent) - 1;
+      $dump .= 'INSERT INTO `' .$table. '` (`' .implode('`, `', $rows). '`) VALUES'.PHP_EOL;
+
+      foreach ($tableContent as $key => $row) {
+        $dump .= '("' .implode('","', array_values($row)). '")';
+        $dump .= ($key == $numberInserts)
+          ? ';' : ','.PHP_EOL;
+      }
+    }
+
+    return array($dump, $numberInserts);
+  }
+
+  /**
+   * Clean old saves from the files
+   */
+  public function cleanup()
+  {
+    $folders = glob($this->storage.'*');
+    foreach($folders as $folder) {
+      list($year, $month, $day) = explode('-', basename($folder));
+      $month = intval($month);
+      $day = intval($day);
+
+      // If dump from last year, remove
+      if($year < date('Y')) {
+        Directory::remove($folder);
+        $this->debug('info', 'Removed save from ' .basename($folder));
+        continue;
+      } else {
+        if($month < date('m') and !in_array($day, array(1, 15))) {
+          Directory::remove($folder);
+          $this->debug('info', 'Removed save from ' .basename($folder));
+          continue;
+        }
+      }
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////
+  /////////////////////// SETTERS AND GETTERS ////////////////////////
+  ////////////////////////////////////////////////////////////////////
+
+  /**
+   * Switch the display of debug messages
+   *
+   * @param boolean $debug Display or not
+   */
+  public function setDebug($debug)
+  {
+    if(!is_bool($debug)) return false;
+
+    $this->debug = $debug;
+  }
+
+  ////////////////////////////////////////////////////////////////////
+  ////////////////////////////// HELPERS /////////////////////////////
+  ////////////////////////////////////////////////////////////////////
+
+  /**
+   * Records a debug message
+   *
+   * @param  string $type    The alert type (info/error/success)
+   * @param  string $message The message
+   * @return string          An Alert message
+   */
+  private function debug($type, $message)
+  {
+    if(!$this->debug) return false;
+
+    echo call_user_func('Alert::'.$type, $message, false);
+  }
+
+  /**
+   * Get the full path to a date's folder
+   *
+   * @param  string $date A date in the format YYYY-mm-dd
+   * @return string       A folder path
+   */
+  private function getFolderForDate($date = null)
+  {
+    if(!$date) $date = $this->date;
+
+    return $this->storage.$date.'/';
+  }
+
+  /**
+   * Get the list of tables in the database
+   *
+   * @return array An array of tables names
+   */
+  private function tables()
+  {
+    $tables = array();
+
+    // Fetch results
+    $sql = "SHOW TABLES FROM `" .DB::connection()->config['database']. "`";
+    $results = $this->pdo->query($sql)->fetchAll(\PDO::FETCH_NUM);
+
+    // Gather table names
+    array_walk($results, function($r) use (&$tables) {
+      $tables[] = $r[0];
+    });
+
+    return $tables;
+  }
+}
--- start.php
+++ start.php
@@ -1,6 +1,10 @@
 <?php
 
-// Setup and starting up ------------------------------------------- /
+/*
+|---------------------------------------------------------------------
+| Setup and loadings
+|---------------------------------------------------------------------
+ */
 
 // Autoload Cerberus
 Autoloader::namespaces(array(
@@ -9,51 +13,17 @@ Autoloader::namespaces(array(
 
 use Cerberus\Toolkit\Buffer,
     Cerberus\Toolkit\Language,
-    Cerberus\Core\Dispatch;
-
-// Cache and builds ------------------------------------------------ /
-
-/*if(Request::method() == 'GET') {
-
-  list($controller, $action) = Cerberus\Core\Navigation::current(true);
-  $identifier = ($controller and $action) ? $controller.'-'.$action : null;
-
-  // Create settings array
-  $cacheSettings = Config::get('cerberus.cache', array());
-
-  // If the page can be cached
-  if($identifier and
-    in_array($identifier, $cacheSettings) or
-    in_array($controller, $cacheSettings)) {
-
-    // Create cache md5
-    $url = Request::method().URL::current();
-    $urlSlug = String::slugify($url);
-    $id = md5($urlSlug);
-
-    // Load cache if found
-    if(Cache::has($id)) {
-      if (!headers_sent()) header('Content-Type: text/html; charset=utf-8');
-      echo Cache::get($id);
-      exit();
-    }
-
-    // Else write cache and display
-    Event::listen('laravel.done', function($id) use ($id)
-    {
-      $content = Buffer::get();
-      Cache::forever($id, $content);
-      echo $content;
-    });
-  }
-}*/
-
-// Language and localization --------------------------------------- /
+    Cerberus\Core\Dispatch,
+    Cerberus\Modules\Backup;
 
 // Set correct language
 $locale = Language::locale();
 
-// Custom Cerberus macros ------------------------------------------ /
+/*
+|---------------------------------------------------------------------
+| Custom macros and validators
+|---------------------------------------------------------------------
+ */
 
 // Favicon
 HTML::macro('favicon', function($favicon)
@@ -83,3 +53,17 @@ Validator::register('not_numeric', function($attribute, $value)
 {
   return preg_match('/^([^0-9]+)+$/i', $value);
 });
+
+/*
+|---------------------------------------------------------------------
+| Database backup
+|---------------------------------------------------------------------
+ */
+
+$backup = new Backup;
+
+// Save database
+$backup->save();
+
+// Remove old saves
+$backup->cleanup();
