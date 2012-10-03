dbd61da45a22ebf5b4cc21e4a0d48474a54bf3bb
Anahkiasen <ehtnam6@gmail.com>
2012-10-03 23:38:44 +0200

    [add] Laravel::title to set a translated title

--- libraries/Toolkit/Laravel.php
+++ libraries/Toolkit/Laravel.php
@@ -8,20 +8,44 @@
 
 namespace Cerberus\Toolkit;
 
+use Lang;
+use Section;
+
 class Laravel
 {
-  public static function translate($key, $fallback = null)
+  /**
+   * Translates a string with fallbacks
+   *
+   * @param  string $key      The key/string to translate
+   * @param  string $fallback A fallback to display
+   * @param  string $lookup   Where in the language files should Cerberus look
+   * @return string           A translated string
+   */
+  public static function translate($key, $fallback = null, $lookup = 'validation.attributes.')
   {
     if(!$fallback) $fallback = $key;
 
     // Search for the key itself
-    $translation = \Lang::line($key)->get(null, '');
+    $translation = Lang::line($key)->get(null, '');
 
     // If not found, search in the field attributes
     if(!$translation) $translation =
-      \Lang::line('validation.attributes.'.$key)->get(null,
+      Lang::line($lookup.$key)->get(null,
       $fallback);
 
     return ucfirst($translation);
+  }
+
+  /**
+   * Attempts to translate a title
+   *
+   * @param  string $title A page or a title
+   * @return string        A title section
+   */
+  public static function title($title = null)
+  {
+    $title = static::translate($title, null, $title.'.login');
+
+    Section::inject('title', $title);
   }
 }
