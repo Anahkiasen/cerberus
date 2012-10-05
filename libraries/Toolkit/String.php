1db81c509510dd0ea665d2187a13284280a669f8
Anahkiasen <ehtnam6@gmail.com>
2012-10-05 13:38:11 +0200

    [del] Remove String::random to avoid duplicate with Str::random

--- libraries/Toolkit/String.php
+++ libraries/Toolkit/String.php
@@ -32,28 +32,6 @@ class String extends \Str
   }
 
   /**
-   * Generate a random string
-   *
-   * @param  integer $length      The length of the generated string
-   * @param  boolean $allowSpaces Whether spaces are allowed or not
-   * @param  boolean $capitalize  Whether the final string should be capitalized
-   * @return string               A random string
-   */
-  public static function random($length = 15, $allowSpaces = true, $capitalize = true)
-  {
-    // Generate random string
-    $spaces = $allowSpaces ? array_fill(0, 5, ' ') : array();
-    $words = array_merge($spaces, range('a', 'z'), range('A', 'Z'));
-    shuffle($words);
-
-    // Create word
-    $word = substr(implode($words), 0, $length);
-    if($capitalize) $word = ucwords(strtolower($word));
-
-    return $word;
-  }
-
-  /**
    * Generates a random date
    *
    * @param  integer $year  The year to use
