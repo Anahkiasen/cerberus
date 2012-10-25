2e093d82b7ecf9154a015d6e5ca2996c95e20b33
Anahkiasen <ehtnam6@gmail.com>
2012-10-25 19:49:35 +0200

    [add] HTML::link_to_home helper

--- macros.php
+++ macros.php
@@ -4,20 +4,36 @@
 /////////////////////////// HTML MACROS //////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// Links helpers --------------------------------------------------- /
 
-// Link in another tab/window
+/**
+ * Creates an HTML link that opens in another tab
+ */
 HTML::macro('blank_link', function($url, $link = null, $attributes = array()) {
   $attributes['target'] = '_blank';
 
   return HTML::link($url, $link, $attributes);
 });
 
-// Favicon
+/**
+ * Creates a link back to the homepage
+ */
+HTML::macro('link_to_home', function($text, $attributes = array()) {
+  return HTML::link(URL::home(), $text, $attributes);
+});
+
+// HEAD helpers ---------------------------------------------------- /
+
+/**
+ * Adds a favicon to the website
+ */
 HTML::macro('favicon', function($favicon) {
   return "<link href='" .URL::to_asset($favicon). "' rel='shortcut icon' />";
 });
 
-// Responsive design
+/**
+ * Adds the base tags for responsive design
+ */
 HTML::macro('responsive', function() {
   $meta  = "<meta name='apple-mobile-web-app-capable' content='yes' />".PHP_EOL;
   $meta .= "<meta name='apple-touch-fullscreen' content='yes' />".PHP_EOL;
@@ -26,7 +42,11 @@ HTML::macro('responsive', function() {
   return $meta;
 });
 
-// Datalist
+// Markup helpers -------------------------------------------------- /
+
+/**
+ * Generates a datalist
+ */
 HTML::macro('datalist', function($name, $list) {
 
   $datalist = '<datalist id="' .$name. '">';
@@ -38,14 +58,18 @@ HTML::macro('datalist', function($name, $list) {
   return $datalist;
 });
 
-// Table add button
+/**
+ * Generates an 'Add item' button
+ */
 HTML::macro('addButton', function($link, $supplementaryClasses = null) {
   $buttonClass = 'block_large_primary_'.$supplementaryClasses.'link';
 
   return Button::$buttonClass(action($link.'@create'), Babel::add($link));
 });
 
-// Table action
+/**
+ * Adds an action column to a table
+ */
 HTML::macro('action', function($link, $icon, $parameters) {
 
   // If we didn't directly pass an array of parameters
