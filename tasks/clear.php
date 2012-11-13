be367eeee127d646c46f116dfe0aa00f853b8df2
Anahkiasen <ehtnam6@gmail.com>
2012-11-13 13:40:18 +0100

    Add patterns for clear:cache and fix deleting of gitignore files

--- tasks/clear.php
+++ tasks/clear.php
@@ -17,20 +17,34 @@ class Cerberus_Clear_Task
   public function cache()
   {
     // If we provided a list of folders to clear
-    $folders = func_get_args();
-    $folders = $folders[0];
+    $arguments = func_get_args();
+    $folders = array_get($arguments, '0.0', '');
+    $folders = explode(',', $folders);
     if (!$folders) $folders = glob(path('storage').'*/');
+
+    // Get pattern
+    $pattern = array_get($arguments, '0.1', null);
+    $pattern = $pattern ? '*'.$pattern.'*' : '*';
+    $cleared = 0;
 
     // List of folders in the storage folder
     foreach ($folders as $folder) {
 
+      // Get folder basename
       $folder = basename($folder);
       if ($folder == 'work') continue;
 
-      File::cleandir(path('storage').$folder);
+      // Clean all the folder, or only certain files
+      $folder = path('storage').$folder;
+      foreach (glob($folder.'/'.$pattern) as $file) {
+        if (basename($file) == '.gitignore') continue;
+        $cleared++;
+        File::delete($file);
+      }
     }
 
-    echo 'The cache was successfully cleared';
+    echo 'The cache was successfully cleared'.PHP_EOL;
+    if ($cleared != 0) echo $cleared. ' files deleted'.PHP_EOL;
   }
 
   /**
@@ -39,6 +53,6 @@ class Cerberus_Clear_Task
   public function run()
   {
     $this->db();
-    $this->cache();
+    $this->cache(array());
   }
 }
