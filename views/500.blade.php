f6cbb1f25638e326dbf6f6fc5653a432aacef1d7
Anahkiasen <ehtnam6@gmail.com>
2012-11-20 00:59:18 +0100

    Add 500 error page

--- views/500.blade.php
+++ views/500.blade.php
@@ -1,12 +1,12 @@
 @layout('name: base')
 
-@section('title')Erreur 404@endsection
+@section('title')Erreur 500@endsection
 
 @section('layout')
   <div class='page-header'>
-    <h1>Vous êtes perdu <small>erreur 404</small></h1>
+    <h1>Problème serveur <small>erreur 500</small></h1>
   </div>
-  <p class='lead'>Il semblerait que vous soyez perdu, ou que vous êtes tombé sur un lien mort.</p>
+  <p class='lead'>Le serveur a subi une erreur durant la récupération de la page</p>
   <p>
     Tentez de revenir à {{ HTML::link(URL::home(), "la page d'accueil") }} et de recommencer. Si l'erreur subsiste nous vous encourageons
     à {{ HTML::mailto('maxime@stappler.fr', 'nous contacter') }} en précisant dans votre email la page
