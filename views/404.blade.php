4202ad6cab45416a582ffed9bd83acfec9c1eb78
Anahkiasen <ehtnam6@gmail.com>
2012-12-01 03:56:32 +0100

    Wrap error views in a stylable block

--- views/404.blade.php
+++ views/404.blade.php
@@ -3,13 +3,15 @@
 @section('title')Erreur 404@endsection
 
 @section('layout')
-  <div class='page-header'>
-    <h1>Vous êtes perdu <small>erreur 404</small></h1>
-  </div>
-  <p class='lead'>Il semblerait que vous soyez perdu, ou que vous êtes tombé sur un lien mort.</p>
-  <p>
-    Tentez de revenir à {{ HTML::link(URL::home(), "la page d'accueil") }} et de recommencer. Si l'erreur subsiste nous vous encourageons
-    à {{ HTML::mailto('maxime@stappler.fr', 'nous contacter') }} en précisant dans votre email la page
-    que vous souhatiez atteindre (<strong>{{ str_replace(URL::base().'/', null, URL::current()) }}</strong>).
-  </p>
+  <section class='error'>
+    <div class='page-header'>
+      <h1>Vous êtes perdu <small>erreur 404</small></h1>
+    </div>
+    <p class='lead'>Il semblerait que vous soyez perdu, ou que vous êtes tombé sur un lien mort.</p>
+    <p>
+      Tentez de revenir à {{ HTML::link(URL::home(), "la page d'accueil") }} et de recommencer. Si l'erreur subsiste nous vous encourageons
+      à {{ HTML::mailto('maxime@stappler.fr', 'nous contacter') }} en précisant dans votre email la page
+      que vous souhatiez atteindre (<strong>{{ str_replace(URL::base().'/', null, URL::current()) }}</strong>).
+    </p>
+  </section>
 @endsection
