b3c91a30d4b4cb25bc809ef63f728a5006747ea8
Anahkiasen <ehtnam6@gmail.com>
2012-12-04 10:13:06 +0100

    Add Elegant->validate method

--- src/Cerberus/Models/Elegant.php
+++ src/Cerberus/Models/Elegant.php
@@ -2,6 +2,7 @@
 namespace Cerberus\Models;
 
 use \Eloquent;
+use \Validator;
 
 abstract class Elegant extends Eloquent
 {
@@ -11,8 +12,26 @@ abstract class Elegant extends Eloquent
    */
   public static $rules = array();
 
+  // Helper methods ------------------------------------------------ /
+
+  /**
+   * Validates input against the model's rules
+   *
+   * @param  array $input The user input
+   * @return Validator
+   */
+  public static function validate($input)
+  {
+    return Validator::make($input, static::$rules);
+  }
+
   // Attributes ---------------------------------------------------- /
 
+  /**
+   * Prints out the model
+   *
+   * @return string [description]
+   */
   public function __toString()
   {
     return (string) $this->name;
