f67cd87e11fb94f6ec2b1a5c50d758aaf544e8f1
Anahkiasen <ehtnam6@gmail.com>
2013-01-10 01:11:53 +0100

    Add Glow.js

---
+++ src/Cerberus/Providers/CerberusServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+namespace Cerberus\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Response;
+use Cerberus\HTML;
+
+class CerberusServiceProvider extends ServiceProvider
+{
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->registerGlow();
+
+    $this->app['html'] = $this->app->share(function($app)
+    {
+      return new HTML($app['url']);
+    });
+  }
+
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return array('html');
+  }
+
+  /**
+   * Register the Glow javascript helper
+   */
+  private function registerGlow()
+  {
+    $app = $this->app;
+
+    $this->app['router']->get('glow.js', function() use ($app) {
+
+      // Get Illuminate's glow
+      $js = $this->app['files']->get('packages/anahkiasen/cerberus/js/glow.js');
+      $js = str_replace('%BASE%', $this->app['url']->getRequest()->root(), $js);
+      $js = str_replace('%ASSET%', $this->app['url']->asset(''), $js);
+
+      // Set correct header
+      $headers['Content-Type'] = 'application/javascript; charset=utf-8';
+
+      return new Response($js, 200, $headers);
+    });
+
+  }
+}
