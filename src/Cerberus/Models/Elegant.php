8c99104870d0c8a60f172b71b5494fbab9acbd6c
Anahkiasen <ehtnam6@gmail.com>
2013-01-15 16:19:03 +0100

    Add Elegant model

---
+++ src/Cerberus/Models/Elegant.php
@@ -0,0 +1,43 @@
+<?php
+namespace Cerberus\Models;
+
+use \Eloquent;
+use \Input;
+use \Validator;
+
+abstract class Elegant extends Eloquent
+{
+  /**
+   * The model's rules
+   * @var array
+   */
+  public static $rules = array();
+
+  // Helper methods ------------------------------------------------ /
+
+  /**
+   * Validates input against the model's rules
+   *
+   * @param  array $input The user input
+   * @return Validator
+   */
+  public static function validate($input = null)
+  {
+    // If no input given, fetch all
+    if (!$input) $input = Input::get();
+
+    return Validator::make($input, static::$rules);
+  }
+
+  // Attributes ---------------------------------------------------- /
+
+  /**
+   * Prints out the model
+   *
+   * @return string [description]
+   */
+  public function __toString()
+  {
+    return (string) $this->name;
+  }
+}
