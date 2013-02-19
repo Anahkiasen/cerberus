2ee0e14a161234a6d09d4705c1a9487bb0f111f0
Anahkiasen <ehtnam6@gmail.com>
2013-02-19 11:25:12 +0100

    Work on Plant

--- src/Cerberus/Plant.php
+++ src/Cerberus/Plant.php
@@ -19,26 +19,20 @@ class Plant extends Seeder
   }
 
   /**
-   * Get the name of the matching Model
-   *
-   * @return string
-   */
-  protected function getTableName()
-  {
-    return String::from(get_called_class())
-      ->remove('Seeder')
-      ->lower()
-      ->obtain();
-  }
-
-  /**
    * Insert data into the database
    *
    * @param array $data Data
    */
-  protected function seed($data)
+  protected function seed($seeds)
   {
     $table = $this->getTableName();
+    $model = $this->getModelName();
+
+    // Format data
+    $seeds = Arrays::each($seeds, function($seed) use ($model) {
+      $seed = new $model($seed);
+      return $seed->getAttributes();
+    });
 
     // Slice for SQLite
     $slicer = floor(999 / sizeof($data[0]));
@@ -48,7 +42,37 @@ class Plant extends Seeder
     }
 
     // Print number of seeds
-    $element = String::remove(get_called_class(), 'Seeder');
+    // $element = String::remove(get_called_class(), 'Seeder');
     // print sizeof($data).' '.lcfirst($element).' seeded successfully'.PHP_EOL;
+  }
+
+  ////////////////////////////////////////////////////////////////////
+  ////////////////////////////// HELPERS /////////////////////////////
+  ////////////////////////////////////////////////////////////////////
+
+  /**
+   * Get the name of the matching Model
+   *
+   * @return string
+   */
+  protected function getModelName()
+  {
+    return String::from(get_called_class())
+      ->remove('Seeder')
+      ->singular()
+      ->obtain();
+  }
+
+  /**
+   * Get the name of the matching table
+   *
+   * @return string
+   */
+  protected function getTableName()
+  {
+    return String::from(get_called_class())
+      ->remove('Seeder')
+      ->lower()
+      ->obtain();
   }
 }
